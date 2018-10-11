72805f80d0e7669d33f61d40a4f6f89ae73d0b42
Kiran <subs.kiran@gmail.com>
2018-10-11 13:31:09 +0530

    sg

--- sessionredirect/main.js
+++ sessionredirect/main.js
@@ -2,45 +2,49 @@ var express = require('express');
 var app = express();
 var session = require('express-session');
 var bodyparser = require("body-parser");
-app.use(bodyparser.urlencoded({extended:false}));
+app.use(bodyparser.urlencoded({ extended: false }));
 
+app.get
 
 app.use(session({
     secret: "kiran", saveUninitialized: true, resave: true
 
 }))
 
+app.use(express.static(__dirname + '/logincss/css'))
+app.use(express.static(__dirname + '/logincss/js'))
+
 
 app.get("/", function (req, res) {
-    res.sendfile("./login.html");
+    res.sendfile("./logincss/login.html");
 
 })
 
-app.post("/login",function(req,res){
-    if(req.session.username !==null){
+app.post("/login", function (req, res) {
+    if (req.session.username !== null) {
 
-        req.session.username= req.body.username;
-        
+        req.session.username = req.body.username;
+
         res.redirect("/home");
     }
 })
 
-function auth(req,res,next){
-    if(!req.session.username){
+function auth(req, res, next) {
+    if (!req.session.username) {
         res.redirect("/")
     }
-else{
-    next();
-}
+    else {
+        next();
+    }
 }
 
-app.get("/home", auth, function(req,res){
-    console.log("Welcome"+req.session.username);
+app.get("/home", auth, function (req, res) {
+    console.log("Welcome" + req.session.username);
     res.sendfile("./home.html")
 })
 
 
-app.get("/logout", function(req,res){
+app.get("/logout", function (req, res) {
     res.session.destroy();
     res.redirect("/");
 })
